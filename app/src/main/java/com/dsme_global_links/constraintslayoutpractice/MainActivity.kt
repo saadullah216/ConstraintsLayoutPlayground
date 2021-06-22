424f504fd7f9f50a8c9b88c96d0058fc3ddc15a0
junaid_shah95 <syed.junaid@otwosoft.com>
2021-06-22 15:29:56 +0500

    respponsive and guidelines

---
+++ app/src/main/java/com/dsme_global_links/constraintslayoutpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dsme_global_links.constraintslayoutpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.Simple_responsive_Screen)
+    }
+}
